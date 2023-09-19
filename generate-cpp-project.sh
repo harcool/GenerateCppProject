67e6928877507f9e4421ae3a9642a5e87ec5485a
harinder <harinderchoudhary1997@gmail.com>
2023-09-19 22:42:43 +0530

    1. Changed comment from // to # 2. Check for options -n or --name 3. If option is not supported throw error 4. Project name should not be empty

--- generate-cpp-project.sh
+++ generate-cpp-project.sh
@@ -8,22 +8,46 @@ Options:
     -n <ProjectName>, --name=<ProjectName> specify the project name [Required]
 "
 
-
-while getopts ":B:p:dsknwlh-:" OPT; do
-    // check for options -n or --name 
-    // If option is not supported throw error 
-    // project name should not be empty 
-
+CURRENT_PATH=$(pwd)
+PROJECT_PARENT_PATH=$(dirname ${CURRENT_PATH})
+while getopts ":n:h-:" OPT; do
+    #OPT would store the flag
+    #OPTARG would store the argument if the flag is followed by : 
+    if [ "$OPT" = "-" ]; then   # long option: reformulate OPT and OPTARG
+        OPT="${OPTARG%%=*}"       # extract long option name
+        OPTARG="${OPTARG#$OPT}"   # extract long option argument (may be empty)
+        OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
+    fi
+    case "${OPT}" in 
+        n | name) 
+        PROJECT_NAME=${OPTARG}
+        ;;
+        h | help)
+        echo "$_usage"
+        exit 0
+        ;;
+        :)
+        echo "It requires an arguement ${OPTARG}"
+        exit 0
+        ;;
+        \?)
+        echo "exiting Illegal Option. Consider using -h flag"
+        exit 0 
+        ;;
+    esac
 done 
 
-// create project 
+if [ $PROJECT_NAME != "" ]; then
+    cd "$PROJECT_PARENT_PATH"
+    mkdir $PROJECT_NAME
+fi
 
-// create main.cpp : TODO P0
-// add boiler plate code to the main.cpp : TODO P0
-// create src directory  : TODO P1
-// create test directory : TODO P1
-// create CMakeLists.txt file for whole project : TODO P0
-// create build directory : TODO P0
-// run cmake -B <pathToBuild> -S <sourceDir> : TODO P0
-// run make : TODO P0
+#create main.cpp : TODO P0
+#add boiler plate code to the main.cpp : TODO P0
+#create src directory  : TODO P1
+#create test directory : TODO P1
+#create CMakeLists.txt file for whole project : TODO P0
+#create build directory : TODO P0
+#run cmake -B <pathToBuild> -S <sourceDir> : TODO P0
+#run make : TODO P0
 
